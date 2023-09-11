3fb1ab0f512315d15f6d6a19cb4dce56f8bfe9b1
jreleaserbot <jreleaser@kordamp.org>
2023-09-11 18:27:46 +0200

    dotcms-cli dotcli1.0.0

--- Formula/dotcms-cli.rb
+++ Formula/dotcms-cli.rb
@@ -1,23 +1,23 @@
-# Generated with JReleaser 1.8.0 at 2023-09-11T18:20:18.392602+02:00
+# Generated with JReleaser 1.8.0 at 2023-09-11T18:27:20.496382+02:00
 class DotcmsCli < Formula
   desc "app -- DotCMS CLI"
   homepage "https://dotcms.com"
-  url "https://github.com/dotCMS/core/releases/download/dotcli1.0.0/cli-1.0.0.jar", :using => :nounzip
+  url "https://github.com/dotCMS/core/releases/download/dotcli1.0.0/dotcli-1.0.0.jar", :using => :nounzip
   version "1.0.0"
-  sha256 "71befe42da9533b90ee59c8e9a7d82d478d14d1087bdcdd9d0ba5b93a6d513e1"
+  sha256 "4ef94d2183d79f043eba0927b063d9648ca6261b4ad957dab0f39eb332e21a34"
   license "Apache-2.0"
 
   depends_on "openjdk@11"
 
   def install
-    libexec.install "cli-1.0.0.jar"
+    libexec.install "dotcli-1.0.0.jar"
 
     bin.mkpath
     File.open("#{bin}/dotcms-cli", "w") do |f|
       f.write <<~EOS
         #!/bin/bash
         export JAVA_HOME="#{Language::Java.overridable_java_home_env(nil)[:JAVA_HOME]}"
-        exec "${JAVA_HOME}/bin/java" -jar #{libexec}/cli-1.0.0.jar "$@"
+        exec "${JAVA_HOME}/bin/java" -jar #{libexec}/dotcli-1.0.0.jar "$@"
       EOS
     end
   end
