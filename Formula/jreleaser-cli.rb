ac2d37e40895c06b7d96b9edcfd906761e629c38
jreleaserbot <jreleaser@kordamp.org>
2023-10-13 16:33:28 +0200

    jreleaser-cli 23.09.3

--- Formula/jreleaser-cli.rb
+++ Formula/jreleaser-cli.rb
@@ -1,29 +1,29 @@
-# Generated with JReleaser 1.8.0 at 2023-09-10T10:19:15.426931+02:00
+# Generated with JReleaser 1.8.0 at 2023-10-13T16:33:22.611666+02:00
 class JreleaserCli < Formula
   desc "app -- Sample Quarkus CLI application"
   homepage "https://github.com/dotCMS/jreleaser-cli"
-  url "https://github.com/dotCMS/jreleaser-cli/releases/download/23.09.1/jreleaser-cli-23.09.1.jar", :using => :nounzip
-  version "23.09.1"
-  sha256 "15547195480cb23f7bde77cd2ab5f28ad265e500f063e5431ab4b58449cdd725"
+  url "https://github.com/dotCMS/jreleaser-cli/releases/download/23.09.3/jreleaser-cli-23.09.3.jar", :using => :nounzip
+  version "23.09.3"
+  sha256 "7d4f5b82055adcb29c384773a8197cda181f79944015f4584e58e60b191cd0b4"
   license "Apache-2.0"
 
   depends_on "openjdk@11"
 
   def install
-    libexec.install "jreleaser-cli-23.09.1.jar"
+    libexec.install "jreleaser-cli-23.09.3.jar"
 
     bin.mkpath
     File.open("#{bin}/jreleaser-cli", "w") do |f|
       f.write <<~EOS
         #!/bin/bash
         export JAVA_HOME="#{Language::Java.overridable_java_home_env(nil)[:JAVA_HOME]}"
-        exec "${JAVA_HOME}/bin/java" -jar #{libexec}/jreleaser-cli-23.09.1.jar "$@"
+        exec "${JAVA_HOME}/bin/java" -jar #{libexec}/jreleaser-cli-23.09.3.jar "$@"
       EOS
     end
   end
 
   test do
     output = shell_output("#{bin}/jreleaser-cli --version")
-    assert_match "23.09.1", output
+    assert_match "23.09.3", output
   end
 end
