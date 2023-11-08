1f1975fe14241d7550b7b62674fc1651364b3aed
jreleaserbot <jreleaser@kordamp.org>
2023-11-08 18:47:40 +0100

    qcli-jbang v1.2.0

--- Formula/qcli-jbang.rb
+++ Formula/qcli-jbang.rb
@@ -1,29 +1,29 @@
-# Generated with JReleaser 1.8.0 at 2023-11-08T18:01:51.184913+01:00
+# Generated with JReleaser 1.8.0 at 2023-11-08T18:47:34.631276+01:00
 class QcliJbang < Formula
   desc "app -- Sample Quarkus CLI application"
   homepage "https://www.dotcms.com"
-  url "https://github.com/dotCMS/qcli-jbang/releases/download/v1.1.0/qcli-jbang-1.1.0.jar", :using => :nounzip
-  version "1.1.0"
-  sha256 "4635e888b3737b43b183d1dabd03a898de8c2684ccaa2a3bba155587d971b958"
+  url "https://github.com/dotCMS/qcli-jbang/releases/download/v1.2.0/qcli-jbang-1.2.0.jar", :using => :nounzip
+  version "1.2.0"
+  sha256 "448ff4f7ac3b3aac098d2f45a4b8112ca2f86ce35e906de59dc0210b59341c39"
   license "Apache-2.0"
 
   depends_on "openjdk@11"
 
   def install
-    libexec.install "qcli-jbang-1.1.0.jar"
+    libexec.install "qcli-jbang-1.2.0.jar"
 
     bin.mkpath
     File.open("#{bin}/qcli-jbang", "w") do |f|
       f.write <<~EOS
         #!/bin/bash
         export JAVA_HOME="#{Language::Java.overridable_java_home_env(nil)[:JAVA_HOME]}"
-        exec "${JAVA_HOME}/bin/java" -jar #{libexec}/qcli-jbang-1.1.0.jar "$@"
+        exec "${JAVA_HOME}/bin/java" -jar #{libexec}/qcli-jbang-1.2.0.jar "$@"
       EOS
     end
   end
 
   test do
     output = shell_output("#{bin}/qcli-jbang --version")
-    assert_match "1.1.0", output
+    assert_match "1.2.0", output
   end
 end
