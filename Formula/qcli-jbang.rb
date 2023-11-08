c5ffa1647e22145cdbcbebff5f95e8a9877bd73b
jreleaserbot <jreleaser@kordamp.org>
2023-11-08 18:02:06 +0100

    qcli-jbang v1.1.0

--- Formula/qcli-jbang.rb
+++ Formula/qcli-jbang.rb
@@ -1,10 +1,10 @@
-# Generated with JReleaser 1.8.0 at 2023-10-19T21:12:17.568578+02:00
+# Generated with JReleaser 1.8.0 at 2023-11-08T18:01:51.184913+01:00
 class QcliJbang < Formula
   desc "app -- Sample Quarkus CLI application"
   homepage "https://www.dotcms.com"
   url "https://github.com/dotCMS/qcli-jbang/releases/download/v1.1.0/qcli-jbang-1.1.0.jar", :using => :nounzip
   version "1.1.0"
-  sha256 "c33f442bec98694a2753033ba5f61b4fb9225a78c57f79cae83ff89adcbed9a0"
+  sha256 "4635e888b3737b43b183d1dabd03a898de8c2684ccaa2a3bba155587d971b958"
   license "Apache-2.0"
 
   depends_on "openjdk@11"
